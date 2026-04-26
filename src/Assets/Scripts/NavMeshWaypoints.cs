b72f99d67a20dbda5025765f75097ef02715aa65
CozyWool <vsergeev201530@gmail.com>
2026-04-26 13:29:03 +0500

    task-2: Задание точек через порядок детей у WaypointHandler

--- src/Assets/Scripts/NavMeshWaypoints.cs
+++ src/Assets/Scripts/NavMeshWaypoints.cs
@@ -1,29 +1,69 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections.Generic;
 
 public class NavMeshWaypoints : MonoBehaviour
 {
-    public Transform[] waypoints;
-    private NavMeshAgent _agent;
-    private int _destPoint = 0;
+    public GameObject waypointsHandler;
 
-    void Start()
+    [Tooltip("Введите номера детей (нумерация с 1) через пробел, например: 0 2 1")]
+    public string orderString = "";
+
+    private readonly List<Transform> _waypoints = new();
+    private NavMeshAgent _agent;
+    private int _destPoint;
+
+    [Header("Status")] [SerializeField] private string currentTargetName; // Поле только для визуализации
+
+    private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
 
-        if (NavMesh.SamplePosition(transform.position, out NavMeshHit hit, 1.0f, NavMesh.AllAreas))
+        InitializeWaypointsByIndices();
+
+        if (NavMesh.SamplePosition(transform.position, out var hit, 1.0f, NavMesh.AllAreas))
         {
             _agent.Warp(hit.position);
         }
 
-        _agent.updateRotation = false;
-
         GoToNextPoint();
     }
 
-    void Update()
+    private void InitializeWaypointsByIndices()
     {
-        if (!_agent.isOnNavMesh)
+        if (waypointsHandler == null || string.IsNullOrEmpty(orderString))
+        {
+            return;
+        }
+
+        var indexStrings = orderString.Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
+
+        var totalChildren = waypointsHandler.transform.childCount;
+
+        foreach (var str in indexStrings)
+        {
+            if (int.TryParse(str, out var index))
+            {
+                if (1 <= index && index <= totalChildren)
+                {
+                    _waypoints.Add(waypointsHandler.transform.GetChild(index - 1));
+                }
+                else
+                {
+                    Debug.LogWarning($"Index {index} out of range! Object has only {totalChildren} children.");
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"'{str}' is not a number!");
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_waypoints.Count == 0 || !_agent.isOnNavMesh)
         {
             return;
         }
@@ -34,14 +74,16 @@ public class NavMeshWaypoints : MonoBehaviour
         }
     }
 
-    void GoToNextPoint()
+    private void GoToNextPoint()
     {
-        if (waypoints.Length == 0)
+        if (_waypoints.Count == 0)
         {
             return;
         }
 
-        _agent.destination = waypoints[_destPoint].position;
-        _destPoint = (_destPoint + 1) % waypoints.Length;
+        _agent.destination = _waypoints[_destPoint].position;
+
+        currentTargetName = $"Index: {_destPoint + 1} | Name: {_waypoints[_destPoint].name}";
+        _destPoint = (_destPoint + 1) % _waypoints.Count;
     }
 }
